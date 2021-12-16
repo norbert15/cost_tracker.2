6726031881e3345ba29d5d5d4e2197f080a2a3ef
Norbert Balogh <norbert.balogh@ad4est.com>
2021-12-16 20:11:15 +0100

    Added spring boot framewrok

---
+++ src/main/java/hu/bnorbi/costtracker/CosttrackerApplication.java
@@ -0,0 +1,13 @@
+package hu.bnorbi.costtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosttrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/bnorbi/costtracker/CosttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bnorbi.costtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
